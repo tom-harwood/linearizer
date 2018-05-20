3d34ccf75c0da2ac38b8f496507d9467a6ddd1bf
Tom Harwood <tharwood@theworld.com>
2018-05-20 10:56:22 -0400

    Initial stub

---
+++ src/java/linearizer/Main.java
@@ -0,0 +1,9 @@
+package linearizer;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.printf("Linearizer version %s\n", linearizer.version.Linearizer.version);
+    }
+}
